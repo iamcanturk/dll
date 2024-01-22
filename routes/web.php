2ae8d9f46e3027cf89a055210904b6edf9834ba0
Yusuf Can TÜRK <iamcanturk@gmail.com>
2024-01-22 16:49:06 +0300

    assets uploaded

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('panel')->group(function () {
+    Route::get ('/login', function () {
+        return view("panel/auth/login");
+   });
+});
+
